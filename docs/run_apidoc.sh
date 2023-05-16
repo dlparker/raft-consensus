9d198ca659a14d3c982b9ea6aafde6bc9828b91f
Dennis Parker <dennis@windhavengroup.com>
2023-05-16 13:00:35 -0500

    tweaking autodoc stuff

--- docs/run_apidoc.sh
+++ docs/run_apidoc.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
-sphinx-apidoc --ext-autodoc -e -M -o source/_autodoc ../raft ../raft/*/tests ../*.py
+#sphinx-apidoc --ext-autodoc -e -M -o source/_autodoc ../raft ../raft/*/tests ../*.py
+cd ..
+sphinx-apidoc --ext-autodoc -e -M -o docs/source/_autodoc raft raft/*/tests 
+
