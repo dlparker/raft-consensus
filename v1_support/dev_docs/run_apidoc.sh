f738c7754740c87d0c74d7ad80811c1490304a94
Dennis Parker <dennis@windhavengroup.com>
2025-02-14 12:09:49 -0600

    removed v1 codee

--- v1_support/dev_docs/run_apidoc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-#sphinx-apidoc --ext-autodoc -e -M -o source/_autodoc ../raftframe ../raftframe/*/tests ../*.py
-cd ..
-sphinx-apidoc --ext-autodoc -e -M -o dev_docs/source/_autodoc dev_tools 
-sphinx-apidoc --ext-autodoc -e -M -o dev_docs/source/_autodoc_bt bank_teller
-
--- v1_support/docs/run_apidoc.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-#sphinx-apidoc --ext-autodoc -e -M -o source/_autodoc ../raftframe ../raftframe/*/tests ../*.py
-cd ..
-sphinx-apidoc --ext-autodoc -e -M -o docs/source/_autodoc raftframe raftframe/*/tests 
-
